34b86744a0f3a504d174dd52708ecc63b36c7d46
Ling Sun <lingsun@mum.edu>
2019-04-20 13:20:06 -0400

    Leetcode 34

--- week1-binary-search/SearchRange.js
+++ week1-binary-search/SearchRange.js
@@ -1,18 +1,49 @@
 var searchRange = function(nums, target) {
-    let low = 0;
-    let high = nums.length -1;
-    
-    while(low +1 < high){
-        let mid = low+(high-low)/2;
-        if(nums[mid]< target){
-            low = mid;
-        }else{
-            high = mid;
-        }
-    }
-    return [low,high];
+    const first = findFirst(nums,target);
+    const last = findLast(nums,target);
+    return [first,last];
     
 };
+function findFirst(nums, target){
+    let low = 0;
+    let high = nums.length -1; 
+    while(low +1 < high){
+        let mid = parseInt(low+(high-low)/2);
+        if(nums[mid]< target){
+            low = mid+1;
+        }else{
+            high = mid;
+        } 
+    }
+    if(nums[low]==target){
+        return low;
+    } 
+    if(nums[high]==target){
+        return high;
+    } 
+    return -1; 
+}
+
+function findLast(nums, target){
+    let low = 0;
+    let high = nums.length -1; 
+    while(low +1 < high){
+        let mid = parseInt(low+(high-low)/2);
+        if(nums[mid]<= target){
+            low = mid;
+        }else{
+            high = mid-1;
+        } 
+    } 
+    if(nums[high]==target){
+        return high;
+    } 
+    if(nums[low]==target){
+        return low;
+    } 
+    return -1; 
+}
+
 const nums = [5,7,7,8,8,10];
 const target = 8 ;
 console.log(searchRange(nums, target));
